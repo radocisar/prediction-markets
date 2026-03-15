35f4c01ce7827473d84cf8b491ada80d8ca0c610
Rado <radocisar@hotmail.com>
2026-03-15 18:47:42 -0400

    fixed typos

--- getdata.py
+++ getdata.py
@@ -18,27 +18,30 @@ GAMMA_RATE = 500  # per 10 secs
 CLOB_RATE = 9000  # per 10 secs
 GAMMA_PAGE_LEN = 100
 
+
 class RateLimiter:
     def __init__(self, rate, rate_window=10):
         self.rate = rate
-        self.window = rate_window
+        self.rate_window = rate_window
         self.lock = threading.Lock()
         self.calls = deque()
 
     def acquire(self):
         while True:
             now = time.time()
-            with self.lock():
+            with self.lock:
                 while self.calls and self.calls[0] <= now - self.rate_window:
                     self.calls.popleft()
                 if len(self.calls) < self.rate:
                     self.calls.append(now)
                     return
-            if wait:=(self.calls[0] + self.rate_window)-now) > 0:
+            if (wait := (self.calls[0] + self.rate_window) - now) > 0:
                 time.sleep(wait)
+
 
 gamma_limiter = RateLimiter(GAMMA_RATE)
 clob_limiter = RateLimiter(CLOB_RATE)
+
 
 def fetch_clob(url):
     clob_limiter.acquire()
@@ -46,11 +49,12 @@ def fetch_clob(url):
     resp = requests.get(url).json().get("asks", ["empty"])[-1]
     return resp.status_code, resp.json()
 
+
 def fetch_gamma(url):
     gamma_limiter.acquire()
     events = requests.get(url)
-    
-    if (evts:=events.json()):
+
+    if evts := events.json():
         # only if there are events returned
         clob_urls = [
             f"https://clob.polymarket.com/book?token_id={clob_token}"
@@ -63,10 +67,14 @@ def fetch_gamma(url):
             fut = [executor.submit(fetch_clob, u) for u in clob_urls]
             for f in as_completed(fut):
                 print(f.result())
-    
+
     return
 
-gamma_urls = [f"https://gamma-api.polymarket.com/events?order=id&ascending=false&active=true&closed=false&limit={GAMMA_PAGE_LEN}&offset={offset}" for offset in range(0, 10,000, GAMMA_PAGE_LEN)]
+
+gamma_urls = [
+    f"https://gamma-api.polymarket.com/events?order=id&ascending=false&active=true&closed=false&limit={GAMMA_PAGE_LEN}&offset={offset}"
+    for offset in range(0, 10000, GAMMA_PAGE_LEN)
+]
 
 with ThreadPoolExecutor(max_workers=500) as executor:
     fut = [executor.submit(fetch_gamma, u) for u in gamma_urls]
